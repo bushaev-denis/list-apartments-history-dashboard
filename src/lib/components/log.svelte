3aefc085efc3878e8b9a3e7c8c60b348b2a6d804
Bushaev Denis <32451886+bushaev-denis@users.noreply.github.com>
2024-04-22 15:31:03 +0000

    fix: logs

--- src/lib/components/log.svelte
+++ src/lib/components/log.svelte
@@ -13,10 +13,12 @@
 
 <div>
 	<h3 class="font-semibold text-xl mb-4">Лог</h3>
-	{#each logs as log}
-		<div class="rounded-xl bg-slate-200 dark:bg-slate-500 px-2 py-4 flex flex-col gap-2">
-			<p>{log.content}</p>
-			<p class="text-xs self-end">{dayjs(log.date).format('DD.MM.YYYY')}</p>
-		</div>
-	{/each}
+	<div class="grid gap-1">
+		{#each logs.reverse() as log}
+			<div class="rounded-xl bg-slate-200 dark:bg-slate-500 px-2 py-4 flex flex-col gap-2">
+				<p>{log.content}</p>
+				<p class="text-xs self-end">{dayjs(log.date).format('DD.MM.YYYY')}</p>
+			</div>
+		{/each}
+	</div>
 </div>
